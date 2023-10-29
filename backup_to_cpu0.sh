8fdb7feb497669c6bc31045cb85ca435c2bf0d83
to0d <to0d@outlook.com>
2023-10-29 22:57:21 +0800

    ch1: add test scripts

---
+++ backup_to_cpu0.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+FR_PROJECT=~/llvm-project
+TO_PROJECT=~/todd-cpu0/src
+
+cd $FR_PROJECT
+
+git status | awk '$1=$1' | grep -e '^modified:' | awk '{print $2}' > modified_list.txt
+
+for modified_file in $(cat modified_list.txt)
+{    
+    fr_path=$FR_PROJECT/$modified_file
+    to_path=$TO_PROJECT/$modified_file
+
+    echo "update: " $modified_file
+    #echo $fr_path
+    #echo $to_path
+
+    mkdir -p $ {dirname $to_path}
+    cp -fr $fr_path $to_path
+}
+
+rm -f modified_list.txt
---
+++ update_to_llvm.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+FR_PROJECT=~/llvm-project
+TO_PROJECT=~/todd-cpu0/src
+
+cp -frv $TO_PROJECT/*  $FR_PROJECT/

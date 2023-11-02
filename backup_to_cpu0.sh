592972e2bf8b0b0c8e54f9bbf345855bb09bc825
to0d <to0d@outlook.com>
2023-11-02 21:57:49 +0800

    update script

--- backup_to_cpu0.sh
+++ backup_to_cpu0.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 FR_PROJECT=~/llvm-project
-TO_PROJECT=~/todd-cpu0/src
+TO_PROJECT=~/llvm-cpu0/src
 
 cd $FR_PROJECT
 
-git status | awk '$1=$1' | grep -e '^modified:' | awk '{print $2}' > modified_list.txt
+git status > _tmp1.txt
 
-for modified_file in $(cat modified_list.txt)
+cat _tmp1.txt | awk '$1=$1' | grep -e '^modified:' | awk '{print $2}' > update_list.txt
+cat _tmp1.txt | awk '$1=$1' | grep -e '^new file:' | awk '{print $3}' >> update_list.txt
+
+for modified_file in $(cat update_list.txt)
 {    
     fr_path=$FR_PROJECT/$modified_file
     to_path=$TO_PROJECT/$modified_file
@@ -17,7 +20,10 @@ for modified_file in $(cat modified_list.txt)
     #echo $to_path
 
     mkdir -p $(dirname $to_path)
-    cp -frv $fr_path $to_path
+    cp -fr $fr_path $to_path
 }
 
-rm -f modified_list.txt
+
+
+rm -f update_list.txt
+rm -f _tmp1.txt
--- update_to_llvm.sh
+++ update_to_llvm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 FR_PROJECT=~/llvm-project
-TO_PROJECT=~/todd-cpu0/src
+TO_PROJECT=~/llvm-cpu0/src
 
 cp -frv $TO_PROJECT/*  $FR_PROJECT/
